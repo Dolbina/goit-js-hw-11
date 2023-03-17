14b841dcefc82fbe991211bc258adbe74d3fc917
Ann Dolbina <dolbinaann@gmail.com>
2023-03-17 22:21:45 +0200

    додає стилі css

--- src/index.js
+++ src/index.js
@@ -18,14 +18,17 @@ const refs = {
 
 let page = 1;
 
-// console.log(refs.searchForm);
-// console.log(refs.input);
-// console.log(refs.button);
-// console.log(refs.gallery);
-// console.log(refs.loadMoreBtn);
-
 refs.searchForm.addEventListener('submit', onSearchImg);
-// refs.loadMoreBtn.addEventListener("click", onLoadMore);
+refs.loadMoreBtn.addEventListener("click", onLoadMore);
+refs.loadMoreBtn.style.display = 'none'; // кнопку не бачимо
+
+
+// колбек функція для кнопки load More
+function onLoadMore () {
+  let name = refs.input.value.trim();
+  page += 1;
+  fetchImg(name, page);
+}
 
 function onSearchImg(event) {
   // відміна перезавантаження сторінки по замовчуванню
@@ -39,47 +42,27 @@ function onSearchImg(event) {
 
   // записує в змінну введене значення користувачем (trim прибирає пробіли)
   let name = form.elements.searchQuery.value.trim();
-  console.log(name);
+ 
 
   // перевірка, якщо значення не пустий рядок
   if (name !== '') {
-    fetchImg(name);
+    fetchImg(name, page);
   } else {
-    refs.loadMoreBtn.style.display = 'none';
-     return Notiflix.Notify.failure(
-       `Sorry, there are no images matching your search query. Please try again.`
-     );
+    refs.loadMoreBtn.style.display = 'none'; // кнопку не бачимо
+    return Notiflix.Notify.failure(
+      `Sorry, there are no images matching your search query. Please try again.`
+    );
   }
 }
-// запрос зображення
-async function fetchImg(name, page) {
-  const API_URL = 'https://pixabay.com/api';
-  const options = {
-    params: {
-      key: '34361382-9628d27261ff8745ccc230a20',
-      q: name,
-      image_type: "photo",
-      orientation: "horizontal",
-      safesearch: true,
-      page: page,
-      per_page: 40,
-    },
-  };
-  try {
-    const response = await axios.get(API_URL, options);
-    insertContent(response.data);
-  } catch (error) {
-    console.log(error);
-  }
-}
-
 
 
 // Розмітка для 1 шт. фото
 const createImg = item =>
   `<a class="photo-link" href="${item.largeImageURL}">
   <div class="photo-card">
+  <div class="photo-wrap">
   <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" />
+  </div>
   <div class="info">
     <p class="info-item">
       <b>Likes</b> ${item.likes}
@@ -105,7 +88,68 @@ const generateContent = (array) => array.reduce((acc, item) => {
 
 // Додавання в DOM
 const insertContent = (array) => {
-    const result = generateContent(array);
-    refs.gallery.insertAdjacentHTML('beforeend', result);
+  const result = generateContent(array);
+  refs.gallery.insertAdjacentHTML('beforeend', result);
+  simpleLightBox.refresh(); // оновлення слайдера lightbox
 };
+
+
+// запрос зображення
+async function fetchImg(name, page) {
+  const API_URL = 'https://pixabay.com/api';
+  const options = {
+    params: {
+      key: '34361382-9628d27261ff8745ccc230a20',
+      q: name,
+      image_type: "photo",
+      orientation: "horizontal",
+      safesearch: true,
+      page: page,
+      per_page: 40,
+    },
+  };
+  try {
+    const response = await axios.get(API_URL, options);
+   
+
+    // якщо користувач ввів не існуючу назву зображення, то відображає повідомлення
+    if (response.data.hits.length === 0) {
+      Notiflix.Notify.failure(
+        `Sorry, there are no images matching your search query. Please try again`
+      );
+      return;
+    }
+
+    if (response.data.totalHits !== 0) {
+      refs.loadMoreBtn.style.display = 'block'; // кнопку бачимо
+    }
+   
+    //  відображає повідомлення з кількістю знайдених зображень при кожному новому запиті
+    if (page === 1) {
+      Notiflix.Notify.success(
+        `Hooray! We found ${response.data.totalHits} images`
+      );
+    }
+    //Якщо користувач дійшов до кінця колекції
+    if (response.data.hits.length < 40) {
+      refs.loadMoreBtn.style.display = 'none'; // кнопку не бачимо
+      Notiflix.Notify.success(
+        `We're sorry, but you've reached the end of search results.`
+      );
+    }
+
+    insertContent(response.data.hits);
+  } catch (error) {
+    console.log(error);
+  }
+}
+// слайдер
+const simpleLightBox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
+
+
+  
   
