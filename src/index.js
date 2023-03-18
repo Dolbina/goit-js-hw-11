0bb5dcfd581693d5814613aa8ae1c388da5ccb0b
Ann Dolbina <dolbinaann@gmail.com>
2023-03-18 14:30:09 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -96,14 +96,14 @@ const insertContent = (array) => {
 
 // запрос зображення
 async function fetchImg(name, page) {
-  const API_URL = 'http://pixabay.com/api';
+  const API_URL = 'https://pixabay.com/api';
   const options = {
     params: {
       key: '34361382-9628d27261ff8745ccc230a20',
       q: name,
-      image_type: "photo",
-      orientation: "horizontal",
-      safesearch: true,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: 'true',
       page: page,
       per_page: 40,
     },
